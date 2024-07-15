b990febce7733ec078979a2867de0e1c78335f5e
JohnQuimson <john.quimson777@gmail.com>
2024-07-15 15:45:59 +0200

    feat: Product card & ratingModule component import

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,6 +16,10 @@ export class HomeComponent {
 
   products: Product[] = [];
 
+  onProductOutput(product: Product) {
+    console.log(product, 'Output');
+  }
+
   ngOnInit() {
     this.productService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
