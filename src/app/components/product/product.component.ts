89ddebf354367fe96bd0d58cb936c81032d0bec0
JohnQuimson <john.quimson777@gmail.com>
2024-07-18 12:51:18 +0200

    feat: confirmDelete function & prime icons

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,21 +3,35 @@ import { Product } from '../../../types';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RatingModule, FormsModule, ButtonModule],
+  imports: [RatingModule, FormsModule, ButtonModule, ToastModule],
+  providers: [ConfirmationService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
 export class ProductComponent {
+  constructor(private confirmationService: ConfirmationService) {}
+
   @Input() product!: Product;
   @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
   @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
 
   editProduct() {
     this.edit.emit(this.product);
+  }
+
+  confirmDelete() {
+    this.confirmationService.confirm({
+      message: 'Sei sicuro di voler eliminare questo prodotto?',
+      accept: () => {
+        this.deleteProduct();
+      },
+    });
   }
 
   deleteProduct() {
